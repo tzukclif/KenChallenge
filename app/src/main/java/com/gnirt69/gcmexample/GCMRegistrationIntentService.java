9693b8eb4a7051fe0b5c2742f65536acb32daf0a
tzukclif <tzuk.clif@gmail.com>
2016-08-08 16:49:40 +0300

    1st upload

---
+++ app/src/main/java/com/gnirt69/gcmexample/GCMRegistrationIntentService.java
@@ -0,0 +1,44 @@
+package com.gnirt69.gcmexample;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+/**
+ * Created by NgocTri on 4/8/2016.
+ */
+public class GCMRegistrationIntentService extends IntentService {
+    public static final String REGISTRATION_SUCCESS = "RegistrationSuccess";
+    public static final String REGISTRATION_ERROR = "RegistrationError";
+
+    public GCMRegistrationIntentService() {
+        super("");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        registerGCM();
+    }
+
+    private void registerGCM() {
+        Intent registrationComplete = null;
+        String token = null;
+        try {
+            InstanceID instanceID = InstanceID.getInstance(getApplicationContext());
+            token = instanceID.getToken(getString(R.string.gcm_defaultSenderId), GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            Log.w("GCMRegIntentService", "token:" + token);
+            //notify to UI that registration complete success
+            registrationComplete = new Intent(REGISTRATION_SUCCESS);
+            registrationComplete.putExtra("token", token);
+        } catch (Exception e) {
+            Log.w("GCMRegIntentService", "Registration error");
+            registrationComplete = new Intent(REGISTRATION_ERROR);
+        }
+        //Send broadcast
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+}
---
+++ app/src/main/java/com/gnirt69/gcmexample/MainActivity.java
@@ -0,0 +1,85 @@
+package com.gnirt69.gcmexample;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.view.Window;
+import android.widget.Toast;
+import android.webkit.WebView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+/**
+ * Created by NgocTri on 4/9/2016.
+ */
+public class MainActivity extends Activity {
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        String url = "https://dailychallenge-1236.appspot.com/";
+        WebView View = (WebView) this.findViewById(R.id.webView);
+        View.getSettings().setJavaScriptEnabled(true);
+        View.loadUrl(url);
+
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //Check type of intent filter
+                if(intent.getAction().equals(GCMRegistrationIntentService.REGISTRATION_SUCCESS)){
+                    //Registration success
+                    String token = intent.getStringExtra("token");
+                    Toast.makeText(getApplicationContext(), "GCM token:" + token, Toast.LENGTH_LONG).show();
+                } else if(intent.getAction().equals(GCMRegistrationIntentService.REGISTRATION_ERROR)){
+                    //Registration error
+                    Toast.makeText(getApplicationContext(), "GCM registration error!!!", Toast.LENGTH_LONG).show();
+                } else {
+                    //Tobe define
+                }
+            }
+        };
+
+        //Check status of Google play service in device
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+        if(ConnectionResult.SUCCESS != resultCode) {
+            //Check type of error
+            if(GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                Toast.makeText(getApplicationContext(), "Google Play Service is not install/enabled in this device!", Toast.LENGTH_LONG).show();
+                //So notification
+                GooglePlayServicesUtil.showErrorNotification(resultCode, getApplicationContext());
+            } else {
+                Toast.makeText(getApplicationContext(), "This device does not support for Google Play Service!", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            //Start service
+            Intent itent = new Intent(this, GCMRegistrationIntentService.class);
+            startService(itent);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "onResume");
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(GCMRegistrationIntentService.REGISTRATION_SUCCESS));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(GCMRegistrationIntentService.REGISTRATION_ERROR));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "onPause");
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+    }
+}
